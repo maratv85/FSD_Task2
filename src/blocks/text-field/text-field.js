4b082d340e4ce2a24cb5ae9d037143674675b7fb
ValievMA <maratv85@gmail.com>
2019-12-19 22:01:18 +0300

    Edit form-elements, text-field

---
+++ src/blocks/text-field/text-field.js
@@ -0,0 +1,11 @@
+/*import 'inputmask/dist/jquery.inputmask.bundle';*/
+
+// $(document).ready(function(){
+//     $(".text-field_input-mask").inputmask({
+//         alias: 'datetime',
+//         inputFormat: 'dd.mm.yyyy',
+//         placeholder: 'ДД.ММ.ГГГГ',
+//         jitMasking: true,
+//         casing: 'upper'
+//         });  
+//     });
