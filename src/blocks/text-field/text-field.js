b03931e91a93b6b1901c41e744e79d6a8192ecd1
ValievMA <maratv85@gmail.com>
2019-12-22 00:13:40 +0300

    Add dropdown block

--- src/blocks/text-field/text-field.js
+++ src/blocks/text-field/text-field.js
@@ -1,11 +1,16 @@
-/*import 'inputmask/dist/jquery.inputmask.bundle';*/
+import IMask from 'imask';
 
-// $(document).ready(function(){
-//     $(".text-field_input-mask").inputmask({
-//         alias: 'datetime',
-//         inputFormat: 'dd.mm.yyyy',
-//         placeholder: 'ДД.ММ.ГГГГ',
-//         jitMasking: true,
-//         casing: 'upper'
-//         });  
-//     });
+var overwriteMask = IMask(
+    document.querySelector('.text-field_date-state'),
+    {
+      mask: Date,
+      lazy: true, /*was false*/
+      overwrite: true, /*was true*/
+      autofix: true,
+      blocks: {
+        d: {mask: IMask.MaskedRange, placeholderChar: 'Д', from: 1, to: 31, maxLength: 2},
+        m: {mask: IMask.MaskedRange, placeholderChar: 'М', from: 1, to: 12, maxLength: 2},
+        Y: {mask: IMask.MaskedRange, placeholderChar: 'Г', from: 1900, to: 2999, maxLength: 4}
+      }
+    }
+  );
