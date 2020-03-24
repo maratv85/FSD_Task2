f1a8ec728ea600ad58f8ea837d4fca06c976aeeb
ValievMA <maratv85@gmail.com>
2020-03-24 23:26:57 +0300

    Edit datepicker block

--- src/blocks/air-datepicker-custom/air-datepicker-custom.js
+++ src/blocks/air-datepicker-custom/air-datepicker-custom.js
@@ -1,2 +1,28 @@
 import 'air-datepicker';
 import 'air-datepicker/dist/css/datepicker.min.css';
+
+$('.air-datepicker-custom').datepicker({
+    startDate: new Date(),
+    prevHtml: '<i class="air-datepicker-custom__material-icon">arrow_back</i>',
+    nextHtml: '<i class="air-datepicker-custom__material-icon">arrow_forward</i>',
+    navTitles: {
+      days: 'MM yyyy',
+      months: 'yyyy',
+      years: 'yyyy1 - yyyy2'},
+    multipleDates: true,
+    multipleDatesSeparator: ' - ' ,
+    range: true
+    })
+
+$.fn.datepicker.language.ru = {
+        days: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+        daysShort: ['Вос', 'Пон', 'Вто', 'Сре', 'Чет', 'Пят', 'Суб'],
+        daysMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+        months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+        monthsShort: ['янв', 'фев', 'мар', 'апр', 'май', 'июн', 'июл', 'авг', 'сен', 'окт', 'ноя', 'дек'],
+        today: 'Сегодня',
+        clear: 'Очистить',
+        dateFormat: 'dd.mm.yyyy',
+        timeFormat: 'hh:ii',
+        firstDay: 1
+      }    
