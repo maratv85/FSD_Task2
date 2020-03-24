f463d88aec65048bbbabd2f7d0beecde6786b637
ValievMA <maratv85@gmail.com>
2020-03-24 20:32:32 +0300

    Add datepicker block

---
+++ src/blocks/air-datepicker-custom/air-datepicker-custom.js
@@ -0,0 +1,2 @@
+import 'air-datepicker';
+import 'air-datepicker/dist/css/datepicker.min.css';
--- src/blocks/text-field/text-field.js
+++ src/blocks/text-field/text-field.js
@@ -10,7 +10,7 @@ var overwriteMask = IMask(
       blocks: {
         d: {mask: IMask.MaskedRange, placeholderChar: 'Д', from: 1, to: 31, maxLength: 2},
         m: {mask: IMask.MaskedRange, placeholderChar: 'М', from: 1, to: 12, maxLength: 2},
-        Y: {mask: IMask.MaskedRange, placeholderChar: 'Г', from: 1900, to: 2999, maxLength: 4}
+        Y: {mask: IMask.MaskedRange, placeholderChar: 'Г', from: 1999, to: 2099, maxLength: 4}
       }
     }
   );
