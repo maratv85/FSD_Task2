3a69be8aebab880d52fb7bb95aae893171266869
ValievMA <maratv85@gmail.com>
2020-03-30 00:00:41 +0300

    Finished Air DatePicker custom plugin

--- src/blocks/air-datepicker-custom/air-datepicker-custom.js
+++ src/blocks/air-datepicker-custom/air-datepicker-custom.js
@@ -1,35 +1,72 @@
 import 'air-datepicker';
 import 'air-datepicker/dist/css/datepicker.min.css';
 
-$('.air-datepicker-custom').datepicker({
-    startDate: new Date(),
-    prevHtml: '<i class="air-datepicker-custom__material-icon">arrow_back</i>',
-    nextHtml: '<i class="air-datepicker-custom__material-icon">arrow_forward</i>',
-    navTitles: {
-      days: 'MM yyyy',
-      months: 'yyyy',
-      years: 'yyyy1 - yyyy2'},
-    multipleDates: true,
-    multipleDatesSeparator: ' - ' ,
-    range: true,
-    clearButton: true,
-    })
+class airDatepickerCustom {
+  constructor(elementDOM) {
+    this.elementDOM = elementDOM;
+    this.initPlugin();
+  }
 
-$.fn.datepicker.language.ru = {
-        days: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
-        daysShort: ['Вос', 'Пон', 'Вто', 'Сре', 'Чет', 'Пят', 'Суб'],
-        daysMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
-        months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
-        monthsShort: ['янв', 'фев', 'мар', 'апр', 'май', 'июн', 'июл', 'авг', 'сен', 'окт', 'ноя', 'дек'],
-        today: 'Сегодня',
-        clear: 'Очистить',
-        dateFormat: 'dd.mm.yyyy',
-        timeFormat: 'hh:ii',
-        firstDay: 1
-      }  
- 
-this.buttonsContainer = document.querySelector('.datepicker--buttons');      
-this.buttonApply = document.createElement('span');      
-buttonApply.classList.add('air-datepicker-custom__apply-button');
-buttonApply.innerText = 'применить';
-buttonsContainer.append(this.buttonApply);
+  initPlugin() {
+    this.initPluginSettings();
+    this.initRegionalDateTimeSettings();
+    this.createApplyButton();
+    this.bindButtonApplyEventListener();
+  }
+
+  initPluginSettings() {
+    $(this.elementDOM).datepicker({
+        startDate: new Date(),
+        prevHtml: '<i class="air-datepicker-custom__material-icon">arrow_back</i>',
+        nextHtml: '<i class="air-datepicker-custom__material-icon">arrow_forward</i>',
+        navTitles: {
+          days: 'MM yyyy',
+          months: 'yyyy',
+          years: 'yyyy1 - yyyy2'},
+        multipleDates: true,
+        multipleDatesSeparator: ' - ' ,
+        range: true,
+        clearButton: true
+        })
+  }      
+  
+  initRegionalDateTimeSettings() {
+    $.fn.datepicker.language.ru = {
+            days: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+            daysShort: ['Вос', 'Пон', 'Вто', 'Сре', 'Чет', 'Пят', 'Суб'],
+            daysMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+            months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+            monthsShort: ['янв', 'фев', 'мар', 'апр', 'май', 'июн', 'июл', 'авг', 'сен', 'окт', 'ноя', 'дек'],
+            today: 'Сегодня',
+            clear: 'Очистить',
+            dateFormat: 'dd.mm.yyyy',
+            timeFormat: 'hh:ii',
+            firstDay: 1
+          }  
+  }          
+  
+  createApplyButton() {
+    this.datepickerContainer = this.elementDOM.querySelector('.datepicker');
+    this.buttonsContainer = this.datepickerContainer.querySelector('.datepicker--buttons');      
+    this.buttonApply = document.createElement('span');      
+    this.buttonApply.classList.add('air-datepicker-custom__apply-button');
+    this.buttonApply.innerText = 'применить';
+    this.buttonsContainer.append(this.buttonApply);
+  }
+
+  _handleApplyButtonClick() {
+    $(this.elementDOM).datepicker().data('datepicker').hide();
+  }
+
+  bindButtonApplyEventListener() {
+    this.buttonApply.addEventListener('click', this._handleApplyButtonClick());
+  }
+
+}
+
+export default airDatepickerCustom
+
+document.addEventListener('DOMContentLoaded', () => {
+  const airDatepickers = document.querySelectorAll('.air-datepicker-custom');
+  airDatepickers.forEach((val) => new airDatepickerCustom(val));
+});
