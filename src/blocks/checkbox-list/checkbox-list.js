62c3a9bef03f9258a3cad695886275097568b2ea
ValievMA <maratv85@gmail.com>
2020-06-18 08:51:23 +0300

    Add outside click event handler to checkbox-list

--- src/blocks/checkbox-list/checkbox-list.js
+++ src/blocks/checkbox-list/checkbox-list.js
@@ -3,17 +3,30 @@ class CheckboxList {
   constructor(elem) {
     this.elem = elem;
     this.init();
-    this.bindClickEvent();
   }
 
   init() {
     this.listArrow = this.elem.querySelector('.js-checkbox-list__arrow');
     this.listHeader = this.elem.querySelector('.js-checkbox-list__header');
     this.listItems = this.elem.querySelector('.js-checkbox-list__items-list');
+    this.bindClickEvent();
+    this.clickOutside();
   }
 
   bindClickEvent() {
     this.listHeader.addEventListener('click', this._handleClick.bind(this));
+  }
+
+  clickOutside() {
+    document.addEventListener('click', (event) => {
+      let isClickInside = this.listItems.contains(event.target);
+      let isClickHeader = this.listHeader.contains(event.target)
+
+      if (!isClickInside && !isClickHeader) {
+        this.listArrow.classList.remove('checkbox-list__arrow_less');
+        this.listItems.classList.add('checkbox-list__items-list_invisible');
+      }    
+    });
   }
 
   _handleClick() {
